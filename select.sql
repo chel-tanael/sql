3e9547a1b511aee31f51054f7b174020ce80d150
Chel Tanael <68893552+chel-tanael@users.noreply.github.com>
2025-08-13 06:47:18 +0000

    Create select SQL

---
+++ select.sql
@@ -0,0 +1,20 @@
+-- Retrieve the table
+SELECT * FROM table_name;
+
+-- Retrieve specific fields from the table
+SELECT column_name1, column_name2 FROM table_name;
+
+-- Retrieve specifc records from the table
+SELECT * FROM table_name WHERE column_name1 = 'value1'
+
+-- Retrieve 
+SELECT DISTINCT
+
+ORDER BY
+
+AND
+
+OR
+
+NOT
+
